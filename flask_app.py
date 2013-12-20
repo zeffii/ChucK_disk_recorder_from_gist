2ce2bf609f7aca1bc417845d5d0df11b99136895
zeffii <tetha.z@gmail.com>
2013-12-20 13:58:31 +0100

    flask app now imports chuck_process
    fingers crossed

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+from chuck_process import take_input
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,12 +8,17 @@ def encoder(path):
     try:
         commands = path.split("&")
         params = {key: value for key, value in [c.split("=") for c in commands]}
+
+        url = params.get('url', "https://gist.github.com/zeffii/8021115")
+        name = params.get('name', "demo_name")
+        time = params.get('time', 30)
+        gain = params.get('gain', 1.0)
+
+        take_input(url, name, int(time), (int(gain)/100))
+        return "yes! " + str(params)
     except:
         return "try that again!"
 
-
-
-    return "yes! " + str(params)
 
 if __name__ == '__main__':
     app.run(debug=True)
